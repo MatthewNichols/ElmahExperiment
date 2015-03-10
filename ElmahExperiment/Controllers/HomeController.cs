3ad2c0e4a281577d07c76adf701d31fc67183b27
Matthew Nichols <matthew@matthew-nichols.com>
2015-03-09 18:27:45 -0600

    Upload project files

---
+++ ElmahExperiment/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ElmahExperiment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ElmahExperiment/Scripts/PageScripts/Index.js
@@ -0,0 +1,39 @@
+﻿$(function() {
+
+    window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+        // Send object with all data to server side log, using severity fatal, 
+        // from logger "onerrorLogger"
+        JL("onerrorLogger").fatalException({
+            "msg": "Unhandled Exception!",
+            "errorMsg": errorMsg, "url": url,
+            "line number": lineNumber, "column": column
+        }, errorObj);
+
+        // Tell browser to run its own error handler as well   
+        return false;
+    }
+
+    $('#throwExplicitError').click(function(e) {
+        JL().fatal("Triggered by click");
+    });
+
+    //$('#throwUnhandledException').click(function(e) {
+    //    var ex = {
+    //        name: 'Very sad error',
+    //        message: 'deep sadness'
+    //    };
+        
+    //    ex.stack = (new Error()).stack;    
+    //    throw ex;
+    //});
+
+    $('#throwUnhandledException').click(function (e) {
+        try {
+            xyz;
+        } catch (e) {
+            JL().fatalException("Handled", e);
+            throw e;
+        } 
+    });
+
+});
